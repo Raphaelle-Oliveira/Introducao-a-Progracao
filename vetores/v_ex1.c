83a16fcefe892aff14386f1d707be7687cacd179
Raphaelle-Oliveira <elleoliveira2017@gmail.com>
2024-10-30 18:09:31 -0300

    Update v_ex1.c

--- vetores/v_ex1.c
+++ vetores/v_ex1.c
@@ -1,27 +1,25 @@
 #include<stdio.h>
-int main(){
-    int n;
-    scanf("%d",&n);
-    int V[n],num_buscas,buscas,i,a;
-    int igual=0;
-    for ( i = 0; i < n; i++){
-        scanf("%d",&V[i]);
-    //printf("vetor %d\n",V[i]);
-    }
-    scanf("%d",&num_buscas);
-    //printf("n de buscas%d\n",num_buscas);
-    for ( a = 0; a < num_buscas; a++){
-        scanf("%d",&buscas);
-        for(i=0 ; i<n ;i++){
-            if (buscas==V[i]){
-                igual++;
-                break;
-            }
-        }
+#include<stdlib.h>
 
-    if(igual==0) printf("NAO ACHEI\n");
-    else printf("ACHEI\n");
-    igual=0;
+int main(){
+    int vetor[100000],busca,aux;
+    int tamanho,i,j,quantBuscas;
+    scanf("%d",&tamanho);
+
+    for(i=0;i<tamanho;i++){
+        scanf("%d",&vetor[i]);
+    }
+
+    scanf("%d",&quantBuscas);
+
+    for(i=0;i<quantBuscas;i++){
+        aux=0;
+        scanf("%d",&busca);
+        for(j=0;j<tamanho;j++){
+            if(busca==vetor[j]) aux++;
+        }
+        if(aux==0) printf("NAO ACHEI\n");
+        else printf("ACHEI\n");
     }
 
     return 0;
